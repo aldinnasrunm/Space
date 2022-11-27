d9eb5fa98fe4d00704a152f45e5fef4a634d9fd2
aldinnasrunm <adnzain40@gmail.com>
2022-11-27 21:53:15 +0700

    adding login functionality using Google Login service

--- app/src/main/java/com/example/space/MainActivity.java
+++ app/src/main/java/com/example/space/MainActivity.java
@@ -2,20 +2,35 @@ package com.example.space;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.space.databinding.ActivityMainBinding;
+import com.example.space.screen.HomeScreenActivity;
+import com.example.space.screen.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding bd;
 
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bd = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(bd.getRoot());
-
+        auth = FirebaseAuth.getInstance();
+        Intent i;
+        //check if user is logged in
+        Log.d("TAG", "Current User " + auth.getCurrentUser());
+        if(auth.getCurrentUser() == null){
+            i = new Intent(MainActivity.this, LoginActivity.class);
+        }else{
+            i = new Intent(MainActivity.this, HomeScreenActivity.class);
+        }
+        startActivity(i);
 
     }
 }
--- app/src/main/java/com/example/space/screen/LoginActivity.java
+++ app/src/main/java/com/example/space/screen/LoginActivity.java
@@ -1,21 +1,80 @@
 package com.example.space.screen;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.space.databinding.ActivityLoginBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding bd;
+    private FirebaseAuth auth;
+    private int REQUEST_CODE_SIGN_IN = 0;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bd = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(bd.getRoot());
+        getSupportActionBar().hide();
         //login activity here
 
+        auth = FirebaseAuth.getInstance();
 
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        bd.btnSiginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignIn();
+            }
+        });
+    }
+
+    private void SignIn() {
+        Intent i = gsc.getSignInIntent();
+        startActivityForResult(i, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+         if(requestCode == 100){
+             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+             try {
+                 task.getResult(ApiException.class);
+                 homeAct();
+             } catch (ApiException e) {
+                 Log.d("TAG", "onActivityResult: " + e.toString());
+                 e.printStackTrace();
+             }
+         }
+    }
+
+    private void homeAct() {
+        finish();
+        Intent i = new Intent(getApplicationContext(), HomeScreenActivity.class);
+        startActivity(i);
     }
 }
